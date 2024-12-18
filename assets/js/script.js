b34348803e17fc17f0fea3171382fa6ed41d9338
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2024-12-18 18:11:42 +0530

    update the cv letter also

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
+// ====== RESUME SECTION ======== 
 function downloadResume() {
     window.print();
 }
+
+
+// ====== COVERLETTER SECTION ======== 
+function downloadCV() {
+    window.print();
+}
+// Function to format date as "DD/MM/YYYY"
+function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
+document.getElementById('date').textContent = formatDate(new Date());
